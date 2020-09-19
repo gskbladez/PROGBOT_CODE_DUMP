4d67f15242d4f7c6e451f37aa9c9c7c8d346099b
rfang-srpg <53104585+rfang-srpg@users.noreply.github.com>
2020-09-19 11:30:47 -0400

    fix power query, move bugreport channel to settings

--- settings.py
+++ settings.py
@@ -23,10 +23,10 @@ backgroundtaskinterval = 10
 
 #MESSAGES
 message_somethingbroke = ":warning::warning: **SOMETHING BROKE** :warning::warning:"
-message_unknowncommand = "Command not recognized"
-message_unhandlederror = "Unhandled error"
-message_resulttoolong = "Sorry, the result was too long to output ({}/2000 characters)"
-message_cooldownactive = "Cooldown active"
-message_restrictedaccess = "You do not have permission to use this command"
+message_unknowncommand = "Command not recognized."
+message_unhandlederror = "Unhandled error."
+message_resulttoolong = "Sorry, the result was too long to output. ({}/2000 characters)"
+message_cooldownactive = "Cooldown active."
+message_restrictedaccess = "You do not have permission to use this command."
 message_oops_failed = "Oops failed!"
 message_oops_success = "Oops success!"
