686b8076fb6aeeb75a846154b9add596a145eb75
rfang-srpg <izaneighi@gmail.com>
2021-07-25 18:41:59 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -27,6 +27,7 @@ pmc_daemonfile = "playermade_daemondata.tsv"
 nyx_chipfile = "nyx_chipdata.tsv"
 nyx_ncpfile = "nyx_powerdata.tsv"
 rulebookfile = "rulebookdata.tsv"
+adventurefile = "adventuredata.tsv"
 
 #BOT SETTINGS
 botname = "Koduck"
