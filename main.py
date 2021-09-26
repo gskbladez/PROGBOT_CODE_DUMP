d08ae680d1e0c6faeea9e444598cb14c0c10c729
Wartorn <wartorncove@yahoo.com>
2021-09-26 00:40:36 -0500

    500 hours in mspaint

--- main.py
+++ main.py
@@ -91,7 +91,8 @@ help_categories = {"Lookups": ':mag: **Lookups**',
                   "Reminders (Base)": ':information_source: **Reminders (Base)**',
                   "Reminders (Advanced Content)": ':trophy: **Reminders (Advanced Content)**',
                   "Reminders (Liberation)": ':map: **Reminders (Liberation)**',
-                  "Reminders (DarkChips)": ':smiling_imp: **Reminders (DarkChips)**'}
+                  "Reminders (DarkChips)": ':smiling_imp: **Reminders (DarkChips)**',
+                  "Safety Tools": ':shield: **Safety Tools**'}
 
 cc_list = list(cc_dict.keys())
 cc_df = pd.DataFrame.from_dict({"Source": cc_list, "Alias": list(cc_dict.values())})
@@ -3043,6 +3044,50 @@ async def achievement(context, *args, **kwargs):
                     value="_{}_".format(achievement_description))
 
     return await koduck.sendmessage(context["message"], sendembed=embed)
+
+async def xcard(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":x: **A participant has used an X-card.** Stop the scene and talk it out.")
+
+async def ncard(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":warning: **A participant has used an N-card.** Consider pausing to talk to your table about the direction of the scene to discuss adjustments.")
+
+async def ocard(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":white_check_mark: A participant has used an O-card. Keep going!")
+
+async def luxton(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":exclamation:**A participant would like to discuss a problem with the current content with the table.** Listen to their needs and wants, and consider giving them control over said content, then continue to play accomodating their requests.")
+
+async def line(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":octagonal_sign: **A participant has discovered a new Line.** You should pause to talk about it, and adjust things as necessary.")
+
+async def veil(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":cloud: **A participant has discovered a new Veil.** Consider toning down the detail in the current scene, and pause to talk to your table about it.")
+
+async def opendoor(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":door: **A participant needs to take a break, stop listening, or leave the game for safety reasons.**")
+
+async def ffwd(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":fast_forward: A participant would like to advance past the current scene.")
+
+async def rewind(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":rewind: A participant would like to rewind certain details of a scene.")
+
+async def pause(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":pause_button: A participant would like to take a break.")
+
+async def fbf(context, *args, **kwargs):
+    return await koduck.sendmessage(context["message"],
+                                    sendcontent=":warning: A participant would like to take it slow during the oncoming scene. Continue as planned with caution.")
 
 def setup():
     koduck.addcommand("updatecommands", updatecommands, "prefix", 3)
