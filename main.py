9b9eeedc424b9ecd6ad26225c7b3613ff8f75759
izaneighi <izaneighi@gmail.com>
2022-01-05 09:53:49 -0500

    fixing dice macro bug

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ SPOTLIGHT_TIMEOUT = datetime.timedelta(days=0, hours=3, seconds=10)
 PROBABLY_INFINITE = 99
 MAX_RANDOM_VIRUSES = 6
 MAX_WEATHER_QUERY = 5
-REROLL_DICE_SIZE_THRESHOLD = 10000000000
+REROLL_DICE_SIZE_THRESHOLD = 1000000000
 MAX_REROLL_QUERY = 20
 MAX_REROLL_QUERY_LARGE = 5
 FORMAT_LIMIT = 175 # technically actually 198 or so, buuuuut
@@ -687,13 +687,18 @@ async def repeatroll(context, *args, **kwargs):
                                         sendcontent="No roll given!")
 
     dice_size = re.search('d(\d+)', roll_line)
-    reroll_size = int(dice_size.group(1))
-    if repeat_arg > MAX_REROLL_QUERY:
-        return await koduck.sendmessage(context["message"],
-                                        sendcontent="Too many small rerolls in one query! Maximum of %d for dice sizes under %d!" % (MAX_REROLL_QUERY, REROLL_DICE_SIZE_THRESHOLD))
-    if repeat_arg > MAX_REROLL_QUERY_LARGE and reroll_size > REROLL_DICE_SIZE_THRESHOLD:
-        return await koduck.sendmessage(context["message"],
-                                        sendcontent="Too many large rerolls in one query! Maximum of %d for dice sizes over %d!" % (MAX_REROLL_QUERY_LARGE, REROLL_DICE_SIZE_THRESHOLD))
+    if not dice_size:
+        dice_size = re.search('(?:E|N|H)(\d+)', roll_line, re.IGNORECASE)
+
+    if dice_size:
+        reroll_size = int(dice_size.group(1))
+
+        if repeat_arg > MAX_REROLL_QUERY:
+            return await koduck.sendmessage(context["message"],
+                                            sendcontent="Too many small rerolls in one query! Maximum of %d for dice sizes under %d!" % (MAX_REROLL_QUERY, REROLL_DICE_SIZE_THRESHOLD))
+        if repeat_arg > MAX_REROLL_QUERY_LARGE and reroll_size > REROLL_DICE_SIZE_THRESHOLD:
+            return await koduck.sendmessage(context["message"],
+                                            sendcontent="Too many large rerolls in one query! Maximum of %d for dice sizes over %d!" % (MAX_REROLL_QUERY_LARGE, REROLL_DICE_SIZE_THRESHOLD))
 
     try:
         roll_heck = [roll_master(roll_line, format_limit=(FORMAT_LIMIT/repeat_arg)) for i in range(0, repeat_arg)]
